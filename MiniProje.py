ca530208c0cd7a1c66cae93750b1b3ce2e645cfd
suleyolalan <yolalansule@hotmail.com>
2025-08-21 04:57:58 +0300

    First
    İlk hafta çözümlerim

---
+++ MiniProje.py
@@ -0,0 +1,25 @@
+#Bölüm3 Mini Proje
+#9.Soru
+"""
+ekmek = int(input("Ekmeğin fiyatı :"))
+peynir = int(input("Peynirin fiyatı :"))
+zeytin = int(input("Zeytinin fiyatı :"))
+toplam_fiyat = ekmek + peynir + zeytin
+
+if toplam_fiyat > 200:
+    yeni_fiyat = toplam_fiyat - (toplam_fiyat * 0.1)
+    print("İndirim kazandınız.Ödeyeceğiniz tutar: ",yeni_fiyat)
+else:
+    print(toplam_fiyat)
+"""
+
+#10.Soru
+yıl = int(input("Hangi klasmandayız öğrenelim. Doğum yılını yazabilir misin? :"))
+yas = int(2025 - yıl)
+
+if 0<=yas<=12:
+    print("Siz bir çocuksunuz")
+elif 13<=yas<=17:
+    print("Siz bir ergensiniz :) ")
+else:
+    print("Siz bir yetişkinsiniz..")
---
+++ Operatörler.py
@@ -0,0 +1,47 @@
+#Bölüm2 Operatörler
+#4.Soru
+"""
+sayi_1 = int(input("1.sayıyı girer misiniz :"))
+sayi_2 = int(input("2.sayıyı girer misiniz :"))
+toplama = print(sayi_1 + sayi_2)
+cıkarma = print(sayi_1 - sayi_2)
+carpma = print(sayi_1 * sayi_2)
+bolme = print(sayi_1 / sayi_2)
+mod_alma = print(sayi_1 % sayi_2)
+"""
+
+#5.Soru
+"""
+ortalama_not = int(input("Notunuzu girer misiniz: "))
+if ortalama_not > 50:
+    print("Geçtiniz")
+else:
+    print("Kaldınız")
+"""
+
+#6.Soru
+"""
+yas = int(input("Yaşınızı öğrenebilir miyim?"))
+if yas > 18:
+    print("Ehliyet alabilirsiniz!!")
+else:
+    print("Ehliyet alamazsınız..")
+"""
+
+#7.Soru
+"""
+urun_fiyat = float(input("Ürünün fiyatını yazabilir misiniz:"))
+ındırım = int(input("İndirim oranını seçiniz: 10 25 40 :"))
+yeni_fiyat = urun_fiyat - (urun_fiyat * (ındırım/100))
+print("Ürünün yeni fiyatı :",yeni_fiyat)
+"""
+
+#8.Soru
+"""
+x = 27
+y = 9
+print(x > y and x > 25)
+print(x > 50 or y < 11)
+print(x < 13 and y > 2)
+print(not(x/y == 5))
+"""
